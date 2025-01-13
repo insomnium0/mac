ef7421c1d8fa74292c1473ca9d0c7a3d23083004
insomnium0 <tsimon1100@gmail.com>
2025-01-13 02:14:17 -0600

    i dont feel like numbering each individual change, so here are all the changes (hopefully):
    removed the yes/no confirmation, instead made it just automatically show you confirmation. free will is an illusion...
    added regex module
    put the code for arguments into a function
    moved and changed the error display message if interface or address are not provided
    edited the "changed mac address" message to sound less excited
    made new code that checks the current mac address, uses regex to single it out, and then prints it
    put all of previously mentioned new code into a function
    changed functionality, so now the program prints your current mac address, both before and after the mac address is changed to the users requested mac address
    removed herobrine

--- mac.py
+++ mac.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-
-
+#
 #                 source code for my 𝓶𝓪𝓬 𝓬𝓱𝓪𝓷𝓰𝓮𝓻 👅
 #       _____   
 #  _   /___  \      Made by: an actual retard
@@ -10,59 +9,80 @@
 # (_) /\___/  /
 #     \______/ 
             
+# adds the module that lets you execute to bash through the python script.
+import subprocess
 
-import subprocess # adds the module that lets you execute to bash through the python script
+# module that adds options and arguments, so instead of having the user input their mac and interface through individual inputs like before, it imports the module to allows them to use arguments such as "-i", "-m", and so on. (more args coming soon!)
+import optparse
 
-import optparse # module that adds options and arguments, so instead of having the user input their mac and interface through individual inputs like before, it imports the module to allows them to use arguments such as "-i", "-m", and so on. (more args coming soon!)
+# a module that adds regex parsing.
+import re
+
+# creates a function that implements arguments into the program, ie. "python mac.py -i wlan0 -m 00:11:22:33:44:55", lets -i be used for interface, and -m for mac address.
+def getargs():
+    # this little piece of code adds an object that can parse arguements, ie. the little "-i" things and whatnot.
+    parser = optparse.OptionParser()
+    # these 2 pieces of code call the parser object, and then calls the method add_option, which adds the option to use shortened args (or full length if you're weird...), and then puts those into the variables address, and interface, respectively.
+    parser.add_option("-i","--interface", dest="interface", help="Interface to change the mac address.")
+    parser.add_option("-m","--mac", dest="address", help="MAC address to change to.")
+    # this now returns the results of parser.parse_args to anything that calls this getargs function.
+    (options, arguments) = parser.parse_args()
+    # displays an error and usage message if the user doesnt input one or the other
+    if not options.interface:
+        print("Error: Interface must be provided.\n Usage: python mac.py -i <interface> -m <mac_address>")
+        parser.error("no interface")
+    elif not options.address:
+            print("Error: MAC address must also be provided.\n Usage: python mac.py -i <interface> -m <mac_address>")
+            parser.error("no mac")
+    # if both above conditions are met, ie both interface and mac are provided, this returns the actual interface and mac address into the "options" variable
+    return options
 
 # creates a function that contains the "heart" of this program; the system commands used to actually change the mac address, for ease of future access.
 def changemac (intr, adr):
-
-# displays an error and usage message if the user doesnt input one or the other
-        if not intr or not adr:
-            print("Error: Both interface and MAC address must be provided.\n Usage: python mac.py -i <interface> -m <mac_address>")
-            exit(1)
-
         # prints that the MAC address is, indeed, about to change.
-        print ('✅ Changing MAC address for '+ intr + " to " + adr + "!\n")
-
+        print ('✅ Changing MAC address for '+ intr + " to " + adr + ".\n")
         # runs the basic shell commands that takes down the interface given, changes the address to whatever you set it to, and then puts it back up.
         subprocess.call(["sudo", "ip", "link", "set", "dev" , intr , "down"],)
         subprocess.call(["sudo", "ip", "link", "set", "dev" , intr , "address" , adr])
         subprocess.call(["sudo", "ip", "link", "set", "dev", intr , "up"])
 
-# this little piece of code adds an object that can parse arguements, ie. the little "-i" things and whatnot.
-parser = optparse.OptionParser()
+# creates a function that gets the current mac address, of the provided interface.
+def getcurrentmac(intr):
+    # stores all the info of the given interface, like mac address, ip address, etc, in the "confirm" variable
+    confirm = subprocess.check_output(["ip", "link",  "show",  "dev", intr])
 
-# these 2 pieces of code call the parser object, and then calls the method add_option, which adds the option to use shortened args (or full length if you're weird...), and then puts those into the variables address, and interface, respectively.
-parser.add_option("-i","--interface", dest="interface", help="Interface to change the mac address.")
-parser.add_option("-m","--mac", dest="address", help="MAC address to change to.")
-# this parses the args, and stores them in options and arguements
-(options, arguments) = parser.parse_args()
-# variables for the interface and address arguments, for future use
+    # decodes any byte-objects within the "confirm" output, rendering it printable
+    confirm_decoded = confirm.decode("utf-8")
+    # uses regex to search through the output of confirm_decoded, specifically only for the mac address.
+    macsearch = re.search(r"ether ([0-9a-f:]{17})", confirm_decoded)
+    # if it was able to find the mac address, it returns the mac address to the line it will be called to, but returns group 1, which omits the "ether" string from appearing, for a cleaner look. if it doesnt find a mac address, it says not found.
+    if macsearch:
+        return macsearch.group(1)
+    else:
+        print ("💔 MAC address not found...")
+     
+# calls the getargs function, and funnels the result into options which renders it usable for the variables below.
+options = getargs()
+
+# calls getcurrentmac, while also assigning it to the currentmac variable, for future use and flexibility. the print statement precedes currentmac with str, so that the numbers of the address may be printed like a string.
+currentmac = getcurrentmac(options.interface)
+print ("🔷 Current MAC address is: " + str(currentmac) + "\n")
+
+# variables for the interface and address arguments, for future use.
 intr = options.interface
 adr  = options.address
 
-# just calls the block of code that actually changes the mac, and accepts interface and mac as inputs
+# just calls the block of code that actually changes the mac, and accepts interface and mac as inputs.
 changemac(intr, adr)
 
-# this part asks if you would like to see confirmation that the address has been changed, so you dont gotta go through the hassle of typing any more convoluted ip commands.
-print("MAC address changed! Would you like to see confirmation to make sure your address has been changed?\n")
-
-# a while loop; basically loops this little section infinitely if anything else other than yes/y or no/n is entered, but it immediately stops the program if either of those are put in.
-while True:
-    choice = input("yes or no? : ").strip().lower()
-
-    if choice in ["yes", "y"]:
-        print("\nYou answered YES! Here it is!\n")
-        subprocess.call(["ip", "link",  "show",  "dev", intr])
-        break
-    elif choice in ["no", "n"]:
-        print("\nYou answered NO! Now DIE!!!\n")
-        break
-    else:
-        print("\nInvalid input. Please enter yes or no. idiot.\n")
-
+# gets the current address again, and confirms that it has been changed. or not if the condition wasnt met.
+currentmac = getcurrentmac(options.interface)
+if currentmac == adr:
+     print("💖 MAC address changed! Confirmation below.\n")
+     print ("🌟 Now your current MAC address is: " + adr + "!")
+else:
+     print ("❌ MAC address was unable to be changed.")
+     
 
 
 
